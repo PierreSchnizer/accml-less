1887b6a987e04a6b557cd61cfca7515f17be545e
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-11-28 09:00:57 +0100

    [TASK] clean up, blackifed examples

--- examples/devices_from_managers/power_converters.py
+++ examples/devices_from_managers/power_converters.py
@@ -13,23 +13,24 @@ logger = logging.getLogger("accml-mml")
 
 def main():
     yp, lm, ts = load_managers()
-    quadrupole_names = yp.get("quadrupoles")
 
-    dvf = DeviceViewFactory(
-        liaison_manager=lm,
-        translator_service=ts
-    )
+    dvf = DeviceViewFactory(liaison_manager=lm, translator_service=ts)
 
     # I deliberately work only with power converters now
     dev_names = list(
-        itertools.chain(*[
-            devices_corresponding_to_element(element_name=quad_name, liaison_manager=lm)
-            for quad_name in yp.get("quadrupoles")
-        ])
+        itertools.chain(
+            *[
+                devices_corresponding_to_element(
+                    element_name=quad_name, liaison_manager=lm
+                )
+                for quad_name in yp.get("quadrupoles")
+            ]
+        )
     )
     dev_views = [dvf.get(name) for name in dev_names]
     dev_views = [view for view in dev_views if view is not None]
     dev_views
+
 
 if __name__ == "__main__":
     main()
